9c3b3fbd37fb8b4e88d1d4800f97960f90f74ac8
Anand Chowdhary <mail@anandchowdhary.com>
2021-02-10 23:18:18 +0530

    :recycle: Add goodreads search

---
+++ src/goodreads.ts
@@ -0,0 +1,35 @@
+import goodreads from "goodreads-api-node";
+
+interface Book {
+  title: string;
+  author: string;
+  image: string;
+  year: number;
+  goodreads: {
+    id: number;
+    ratingsCount: number;
+    averageRating: number;
+    authorId: number;
+  };
+}
+
+export const search = async (key: string, secret: string, q: string): Promise<Book> => {
+  const gr = goodreads({ key, secret });
+  const results = await gr.searchBooks({ q });
+  const result = results.search.results.work.sort(
+    (a, b) => parseInt(a.ratings_count._) - parseInt(b.ratings_count._)
+  )[0];
+  if (!result) throw new Error("Book not found");
+  return {
+    title: result.best_book.title,
+    author: result.best_book.author.name,
+    image: result.best_book.image_url,
+    year: parseInt(result.original_publication_year._),
+    goodreads: {
+      id: parseInt(result.best_book.id._),
+      ratingsCount: parseFloat(result.ratings_count._),
+      averageRating: parseFloat(result.average_rating),
+      authorId: parseInt(result.best_book.author.id._),
+    },
+  };
+};
