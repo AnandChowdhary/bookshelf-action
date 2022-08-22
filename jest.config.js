1fc10371669c43d554313387815691930a1360dc
Anand Chowdhary <mail@anandchowdhary.com>
2022-08-22 12:32:10 +0530

    :wrench: Use common jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  roots: ["<rootDir>/src/"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  automock: false,
 };
